ab39108b0e8fd6f75a4d2e8e208a56212e2587b0
Chi Nguyen <28468296+chinguyen7@users.noreply.github.com>
2023-02-26 11:14:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -55,3 +55,26 @@ document.body.style.filter = "blur(1rem)";
      await sleep(70);
     document.body.style.filter = "blur(0)";
 }
+
+function cacheImages(array)
+{
+    if (!cacheImages.list) {
+        cacheImages.list = [];
+    }
+    var list = cacheImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+cacheImages(["background.jpg" ]);
